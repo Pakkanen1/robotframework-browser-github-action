ff6971c9e02078e657b55c088969eae0aa241b28
Antti Pakkanen <bonetailur@gmail.com>
2021-12-17 15:16:39 +0200

    Update run-browser-tests.sh

--- run-browser-tests.sh
+++ run-browser-tests.sh
@@ -1,6 +1,8 @@
+dir=$(dirname $0)
+echo $dir/seccomp_profile.json
 docker run --rm \
  -v $TESTS_DIR:/test \
  --user pwuser \
- --security-opt seccomp=seccomp_profile.json \
+ --security-opt seccomp=$dir/seccomp_profile.json \
  $IMAGE:$IMAGE_VERSION \
  bash -c "$ROBOT_COMMAND"
