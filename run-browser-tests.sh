bb343a4761536c669efa5f449c748c17132f406f
Antti Pakkanen <bonetailur@gmail.com>
2021-12-17 17:24:37 +0200

    Update run-browser-tests.sh

--- run-browser-tests.sh
+++ run-browser-tests.sh
@@ -1,6 +1,6 @@
 dir=$(dirname $0)
 docker run --rm \
- -v $TESTS_DIR:/home/pwuser/test \
+ -v $TESTS_DIR:/test \
  --user pwuser \
  --security-opt seccomp=$dir/seccomp_profile.json \
  $IMAGE:$IMAGE_VERSION \
