0ab20df9824235ba0c415087585cea04775bf0eb
Antti Pakkanen <bonetailur@gmail.com>
2021-12-17 15:11:49 +0200

    Update run-browser-tests.sh

--- run-browser-tests.sh
+++ run-browser-tests.sh
@@ -1,6 +1,6 @@
 docker run --rm \
  -v $TESTS_DIR:/test \
- -ipc=host --user pwuser \
+ --user pwuser \
  --security-opt seccomp=seccomp_profile.json \
  $IMAGE:$IMAGE_VERSION \
  bash -c "$ROBOT_COMMAND"
